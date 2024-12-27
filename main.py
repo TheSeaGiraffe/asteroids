adf54643ee1d5c166d9607e31a339d62018eb431
Fahmi Adi Nugraha <fnugraha@gmail.com>
2024-12-27 13:25:54 +0700

    Add the beginnings of the main game loop to main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pygame
 
-from constants import (
-    ASTEROID_KINDS,
-    ASTEROID_MAX_RADIUS,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_SPAWN_RATE,
-    SCREEN_HEIGHT,
-    SCREEN_WIDTH,
-)
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 def main():
+    num_pass, num_fail = pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen_coords = screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
