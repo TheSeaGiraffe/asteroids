aa3c4ff1c833f38d01862a3caefe89a67c797e0e
Fahmi Adi Nugraha <fnugraha@gmail.com>
2024-12-27 13:46:24 +0700

    Add a clock object to cap the game's FPS to 60

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     print("Starting asteroids!")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen_coords = screen.fill((0, 0, 0))
         pygame.display.flip()
+        time_since_last_tick = clock.tick(60)
+        dt = time_since_last_tick / 1000
 
 
 if __name__ == "__main__":
