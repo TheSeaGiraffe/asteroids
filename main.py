f9b243fde1ea658dd48f7d1946fafa9e5443e91a
Fahmi Adi Nugraha <fnugraha@gmail.com>
2024-12-29 14:31:34 +0700

    Update main.py to use pygame groups

--- main.py
+++ main.py
@@ -9,23 +9,30 @@ def main():
     num_pass, num_fail = pygame.init()  # Don't do anything with return values just yet
     print("Starting asteroids!")
 
-    # Init screen and player
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    x, y = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
-    player = Player(x, y)
-
     # Init clock
     clock = pygame.time.Clock()
     dt = 0
+
+    # Create pygame groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Init screen and player
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x, y = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
+
+    Player.containers = (updatable, drawable)
+    Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # screen_coords = screen.fill((0, 0, 0))
         _ = screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        for d in drawable:
+            d.draw(screen)
+        for u in updatable:
+            u.update(dt)
         pygame.display.flip()
         time_since_last_tick = clock.tick(60)
         dt = time_since_last_tick / 1000
